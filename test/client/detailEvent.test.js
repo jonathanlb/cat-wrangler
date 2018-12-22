71092cb969dd0020eb153dbfdfede345fc7ea9e4
Jonathan Bredin <bredin@acm.org>
2018-12-22 10:53:37 -0600

    event detail evergreen test

--- test/client/detailEvent.test.js
+++ test/client/detailEvent.test.js
@@ -4,25 +4,51 @@
 
 const detailEvent = require('../../src/client/views/detailEvent');
 
+const userInfo = {
+  1: {
+    id: 1,
+    name: 'Bilbo',
+    section: 'Hobbit',
+  },
+  2: {
+    id: 2,
+    name: 'Frodo',
+    section: 'Hobbit',
+  },
+  3: {
+    id: 3,
+    name: 'Gandolf',
+    section: 'Wizard',
+  },
+  4: {
+    id: 4,
+    name: 'Smaug',
+    section: 'Dragon',
+  },
+};
+
 describe('Event detail component', () => {
   test('renders', () => {
     const details = {
-      11: { },
-      12: { }
+      21: {
+        1: 1, 2: 0, 3: -1, 4: 0,
+      },
+      22: { },
     };
     const app = {
-      getEventDetails: async (id) => details,
-    }
+      getEventDetails: async () => details,
+      getUserInfo: async id => userInfo[id],
+    };
     const viewOpts = {
       event: {
         id: 1,
-        name: 'Testing Huzzah'
+        name: 'Testing Huzzah',
       },
       dt: {
         id: 21,
         yyyymmdd: '20181225',
         hhmm: '1300',
-        duration: '120m'
+        duration: '120m',
       },
     };
     const elt = detailEvent(app, viewOpts);
@@ -30,5 +56,4 @@ describe('Event detail component', () => {
     document.body.innerHTML = '';
     document.body.append(elt);
   });
-
 });
