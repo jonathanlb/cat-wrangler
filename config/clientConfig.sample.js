de0216906d297379615bf59d88ca0e7d35203385
Jonathan Bredin <bredin@acm.org>
2019-01-10 17:08:51 +0000

    add sample config files

---
+++ config/clientConfig.sample.js
@@ -0,0 +1,8 @@
+// Local client configuration options.
+// 'npm run config' will write this file to config/clientConfig.js
+// 'npm run unconfig' will restore this file using git.
+
+module.export = {
+  serverPrefix: 'https://your.host.com:3000',
+  title: 'Your site title for login'
+};
---
+++ config/serverConfig.sample.js
@@ -0,0 +1,25 @@
+// Local server configuration options.
+// 'npm run config' will write this file to config/serverConfig.js
+// 'npm run unconfig' will restore this file using git.
+
+module.exports = {
+  allowCORS: true,
+  email: 'admin@your.host.com',
+  httpPort: 3010, // leave undefined to force https
+  httpsOpts: { // leave undefined if you cannot run https
+    caFile: './config/chain.pem',
+    certFile: './config/server.crt.pem',
+    keyFile: './config/server.key.pem',
+    port: 3011,
+  },
+  mailConfig: {
+    sendmail: true,
+    newline: 'unix',
+    path: '/usr/sbin/sendmail',
+  },
+  siteTitle: 'Title for site header',
+  siteURL: 'http://your.host.com', // used for password reset email
+  sqliteTimekeeper: {
+    file: 'data/db.sqlite3',
+  },
+}
